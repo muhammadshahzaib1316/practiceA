0c062019ae3205d14ab2adae5a91fba70e2e6eb7
muhammadshahzain1316 <shahzaib012khan.com>
2025-04-13 14:04:20 +0500

    list working

--- main.dart
+++ main.dart
@@ -1,1 +1,34 @@
-void main()
+// void main() {
+//   String email = "shahzaib12";
+//   String password = "asdfg";
+//   if (email == "shahzaib12") {
+//     if (password == "asdfg") {
+//       print('login success ful');
+//     } else {
+//       print("invalid password");
+//     }
+//   } else {
+//     print('invalid email');
+//   }
+// // }
+
+// list
+void main() {
+  print("========list========");
+  List stdName = ['ali', 'zara', "anzish", "fatima"];
+  List stdnamE = ["ali", 12, 1.3]; // already dynamic
+  List<dynamic> stdname = ["ali", 12, 1.3];
+  print(stdName[2]);
+  print(stdName.length);
+  stdName.add("hamza");
+  print(stdName);
+  print(stdName.length);
+  stdName.addAll(["louvaiza", 'balaj', 'abass']);
+  print(stdName);
+  print(stdName.length);
+} 
+
+// void  main() 
+// { git 
+//   String?
+// }
