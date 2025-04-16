dab3e054d06bf6c79081fa80fc9f2c038ed6155b
muhammadshahzain1316 <shahzaib012khan.com>
2025-04-17 00:25:51 +0500

    assigment #1

--- main.dart
+++ main.dart
@@ -1,34 +1,55 @@
-// void main() {
-//   String email = "shahzaib12";
-//   String password = "asdfg";
-//   if (email == "shahzaib12") {
-//     if (password == "asdfg") {
-//       print('login success ful');
-//     } else {
-//       print("invalid password");
-//     }
-//   } else {
-//     print('invalid email');
-//   }
-// // }
-
-// list
 void main() {
-  print("========list========");
-  List stdName = ['ali', 'zara', "anzish", "fatima"];
-  List stdnamE = ["ali", 12, 1.3]; // already dynamic
-  List<dynamic> stdname = ["ali", 12, 1.3];
-  print(stdName[2]);
-  print(stdName.length);
-  stdName.add("hamza");
-  print(stdName);
-  print(stdName.length);
-  stdName.addAll(["louvaiza", 'balaj', 'abass']);
-  print(stdName);
-  print(stdName.length);
-} 
+  print('             INTRODUCTION \n i am Programmer ');
+  String Name = 'my name is shahzaib';
+  int Age = 20;
+  String Class = 'flutter';
+  double Per = 33.3;
+  print(Name);
+  print('my age is $Age');
+  print(' I am Still learning $Class in class');
+  print(' i Achieve $Per percentage ');
 
-// void  main() 
-// { git 
-//   String?
-// }
+  // operator
+  //   arthematic operator
+  int a = 20;
+  int b = 24;
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a % b);
+  print(a / b);
+  print(a ~/ b);
+  //   EQULITY &  RELATIONAL OPERATOR
+
+  int num1 = 30;
+  int num2 = 40;
+
+  print(num1 > num2); //num1 greater check >
+  print(num1 < num2); //num1 less check <
+  print(num1 <= num2); //num1 less or equal check <=
+  print(num1 >= num2); //num1 greater or equal check >=
+  print(num1 == num2); // Equal
+  print(num1 != num2); // not Equal
+
+  int numone = 30;
+  int numtwo = 40;
+  int numthree = 30;
+  int numfour = 40;
+
+  print(numone > numtwo && numthree < numfour); // both same true
+  print(!(numone > numtwo && numthree < numfour)); // both same true
+  print(!(!(numone > numtwo && numthree < numfour))); // both same true
+  print(numone < numtwo && numthree > numfour); // both same true
+  print(numone < numtwo && numthree < numfour); // both same true
+  print(numone > numtwo && numthree > numfour); // both same true
+  print(numone > numtwo && numthree > numfour); // both same true
+  print(!(numone > numtwo && numthree < numfour)); // both same true
+
+  print(numone > numtwo || numthree < numfour); // >< both false is true
+  print(!(numone > numtwo || numthree < numfour));
+  print(!(!(numone > numtwo || numthree < numfour)));
+  print(numone < numtwo || numthree > numfour); // <> oth false is true
+  print(numone < numtwo || numthree < numfour); // one   check
+  print(numone > numtwo || numthree > numfour); // >>  false
+  print(!(numone > numtwo || numthree > numfour)); // one  check
+}
