40a2098becf9e16ba4f278c62f776c0073877ffd
muhammadshahzain1316 <shahzaib012khan.com>
2025-04-19 17:13:54 +0500

    assigment two

---
+++ asstwo main.dart
@@ -0,0 +1,110 @@
+//   //           ASSIGMENT # 2
+//                //  part 1
+void main() {
+  print('hello i am a converter \n i convert fahrenheit to celsius');
+  print('fahrenheit value is 98');
+  double cel = (98 - 32) * 5 / 9;
+  print(' celsius value is $cel');
+}
+                                     // part 2
+  // print ('i am find area of rectangle');
+  // print ('breadth value is 5 cm ');
+  // print ( ' length value is 7 cm ');
+  // print (' now we find area of rectangle' );
+  // int bredth = 5;
+  // int lenght = 7;
+  // int area = lenght * bredth;
+  // print (' area of rectangle value is $area cm');
+
+//                                  //part 3
+
+//   int num1 = 7;
+//   int num2 = num1 + 8;
+//   double num3 = num2 / 3;
+//   double num4 = num3 % 5;
+//   double num5 = num4 * 5;
+//   double total =num5;
+//   print (total);
+
+//                                  // part 4
+
+//   print  ( ' Three subject each out of 100' ) ;
+//   int math = 78;
+//   double permath = math / 100 *100;
+//   print ('Maths marks is  $math ');
+//   print ('Maths subject percentage is $permath%');
+//   int english = 45;
+//   double perenglish = english / 100 *100;
+//   print ('English marks is  $english ');
+//   print ('English subject percentage is $perenglish%');
+//   int computer = 62;
+//   double percomputer = computer / 100 *100;
+//   print ('Computer marks is $computer ');
+//   print ('Computer subject percentage is $percomputer%');
+//   int total = math + english + computer;
+//   print ('you achieve three subject marks is $total ');
+//   double per = total / 300 *100;
+//   print ('you achieve three subject percentage is $per%');
+// }
+
+// MARKSSHEET
+//   void main() {
+//   int englishmarks = 40;
+//   int mathsmarks = 30;
+//   int computermarks = 50;
+//   int physicmarks = 50;
+//   print('===========================================================');
+//   print('                 marks sheet               ');
+//   print('===========================================================');
+
+//   print('you achieve english marks is $englishmarks             out of 50');
+//   double pere = englishmarks * 100 / 50;
+
+//   double perm = mathsmarks * 100 / 50;
+
+//   double perc = computermarks * 100 / 50;
+
+//   double perp = physicmarks * 100 / 50;
+//   print('you achieve  english percentage is $pere%       out of 100%');
+//   print('you achieve  maths marks is $mathsmarks               out of 50');
+//   print('you achieve  maths  percentage is $perm%        out of 100%');
+//   print('you achieve  computer marks is $computermarks            out of 50');
+//   print('you achieve  computer percentage is $perc%     out of 100%');
+//   print('you achieve  physic marks is $physicmarks              out of 50');
+//   print('you achieve  physic percentage is $perp%       out of 100%');
+//   int total = englishmarks + mathsmarks + computermarks + physicmarks;
+//   double totalper = total * 100 / 200;
+//    print('===========================================================');
+//   print('you achieve  total percentage is $totalper%        out of 100%');
+//   print('you achieve  total marks is $total              out of 200');
+//    print('===========================================================');
+//   if (total == 200)
+//   {
+//     print('===========================================================');
+//     print('you are selected');
+//     print('you are a good student');
+//     print('your grade is A+');
+//     print('===========================================================');
+//     print('===========================================================');
+//   } else if (total >= 150)
+//   {
+//     print('===========================================================');
+//     print('you are selected');
+//     print('you need some efforts');
+//     print('your grade is A');
+//     print('===========================================================');
+//   } else if (total <= 100)
+//   {
+//     print('===========================================================');
+//     print('you are selected');
+//     print('you need hard efforts');
+//     print('your grade is B');
+//     print('===========================================================');
+//   } else {
+//     print('===========================================================');
+//     print('you are not  selected');
+//     print('you need hard efforts');
+//     print('your grade is fail');
+//     print('===========================================');
+//   }
+// }
