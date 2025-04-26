9919df7f640818c9f7407aebce450f978d92947c
muhammadshahzaib1316 <shahzaib012khan@gmail.com>
2025-04-26 14:50:48 +0500

    leap year

---
+++ leapyear.dart
@@ -0,0 +1,9 @@
+void main() {
+  print(' HELLO \n i will tell this is leap year \ non leap year ');
+  int year = 2000;
+  if (year % 4 == 0 || year % 100 == 0 && year % 400 == 0) {
+    print('$year is a leap year');
+  } else {
+    print('$year is not leap year');
+  }
+}
